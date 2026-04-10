1999dd198a3ce4bd90ab9a631ba60e36cebbe641
vitor-nery11 <vitornerysantos24@gmail.com>
2026-04-09 23:12:56 -0300

    implementando ver tarefas

--- gerenciador.py
+++ gerenciador.py
@@ -1,13 +1,18 @@
 def adicionar_tarefas(lista_tarefas,nome_tarefa):
-
-#tarefa: vai armazenar o nome da tarefa
-#completa: vai indicar se tarefa foi completada
   tarefa = {"nome": nome_tarefa, "completada":False}
   lista_tarefas.append(tarefa)
   print(f"tarefa {nome_tarefa} foi adicionada com sucesso!")
   return 
 
+def ver_tarefas(lista_tarefas):
+  print("\nLista de tarefas:\n")
+  
+  for indice, tarefa in enumerate(lista_tarefas):
+    status = "✓" if tarefa["completada"] else " "
+    print(f"{indice}. [{status}] {tarefa['nome']}")
+
 lista_tarefas = []
+
 
 while True:
   print("\n Menu do gerenciador lista de tarefas:")
@@ -23,6 +28,8 @@ while True:
   if escolha == "1":
     nome_tarefa = input("Digite o nome da tarefa que deseja adicionar:")
     adicionar_tarefas(lista_tarefas,nome_tarefa)
+  elif escolha == "2":
+     ver_tarefas(lista_tarefas)
   elif escolha == "6":
     break
 
